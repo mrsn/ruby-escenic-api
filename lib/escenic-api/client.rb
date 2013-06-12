a01b9463d7eede3237ea6482dcc382514feb3108
elisarver <eli.sarver@gmail.com>
2013-06-12 13:30:44 -0400

    Move all configuration into config module.

--- lib/escenic-api/client.rb
+++ lib/escenic-api/client.rb
@@ -5,10 +5,6 @@ module Escenic
       attr_reader :raw
 
       def initialize
-        Escenic::API::Config.base_model = Escenic::API::Config.base_url     + '/publication/'   +
-                                          Escenic::API::Config.publication  + '/escenic/model'
-        Escenic::API::Config.endpoint   = Escenic::API::Config.base_url     + '/escenic'
-
         @raw = Escenic::API::Raw.new
       end
 
--- spec/support/config.rb
+++ spec/support/config.rb
@@ -1,11 +1,15 @@
 require 'yaml'
 
-GET_ROOT_XML        = File.new 'spec/xml/get_root.xml', 'r'
-GET_SECTION_XML     = File.new 'spec/xml/get_section.xml', 'r'
-CREATE_SECTION_XML  = File.new 'spec/xml/create_section.xml', 'r'
+GET_ROOT_XML       = File.new 'spec/xml/get_root.xml', 'r'
+GET_SECTION_XML    = File.new 'spec/xml/get_section.xml', 'r'
+CREATE_SECTION_XML = File.new 'spec/xml/create_section.xml', 'r'
 
-config = YAML.load_file 'spec/connection.yml'
-Escenic::API::Config.base_url     = config['base_url']
-Escenic::API::Config.user         = config['user']
-Escenic::API::Config.pass         = config['pass']
-Escenic::API::Config.publication  = config['publication']
+config                           = YAML.load_file 'spec/connection.yml'
+Escenic::API::Config.base_url    = config['base_url']
+Escenic::API::Config.user        = config['user']
+Escenic::API::Config.pass        = config['pass']
+Escenic::API::Config.publication = config['publication']
+Escenic::API::Config.base_model  = Escenic::API::Config.base_url + '/publication/' +
+    Escenic::API::Config.publication + '/escenic/model'
+Escenic::API::Config.endpoint    = Escenic::API::Config.base_url + '/escenic'
+
